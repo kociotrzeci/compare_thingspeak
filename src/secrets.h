32ab9513421ae715d56aeb03170cbbe0e8a28c8d
kociotrzeci <kociotrzeci@gmail.com>
2023-06-08 18:17:23 +0200

    rm secrets

--- src/secrets.h
+++ src/secrets.h
@@ -1,5 +1,4 @@
-#define SECRET_SSID "SuperWiFi"
-#define SECRET_PASSWD "ArciaArcia!234"
-#define SECRET_MQTT_PASSWD "mqtt3.thingspeak.com"
-#define counterChannelNumber 865129
-#define SECRET_READ_API "J90M1D8KVRXJNNFF"
+#define SECRET_SSID "ssid"
+#define SECRET_PASSWD "pass"
+#define counterChannelNumber 123
+#define SECRET_READ_API "apik"
